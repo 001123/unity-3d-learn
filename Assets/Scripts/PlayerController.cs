f8d5339940cfea393a5411be66dac7f712548769
duy.bui <duy.bui@hellohealthgroup.com>
2020-09-08 21:12:08 +0700

    Init roll a ball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public GameObject restartBtnObj;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    private int count;
+    private float timer;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        timer = 0;
+
+        SetCountText();
+
+        winTextObject.SetActive(false);
+        restartBtnObj.SetActive(false);
+
+        //Start the coroutine we define below named ExampleCoroutine.
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    private void Update()
+    {
+    }
+
+    void SetCountText()
+    {
+        countText.text = "😎 Score: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+            restartBtnObj.SetActive(true);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            count++;
+            other.gameObject.SetActive(false);
+            SetCountText();
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/RestartBtn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class RestartBtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ClickRestart()
+    {
+        Debug.Log("Click");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
